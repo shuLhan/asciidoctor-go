19e2b864ea304f6ab7bd92e97fb1017988f527a7
Shulhan <ms@kilabit.info>
2022-07-24 02:30:28 +0700

    all: rewrite test using lib/test.Data
    Previously, to test parser and check the generated HTML, we write AsciDoc
    input and expected HTML output using literal string `...`.
    The text of input and output sometimes long, take multiple lines, which
    makes the test code ugly, hard to write, and read.
    
    Using lib/test.Data we can write the input and output as the AsciiDoc
    markup and the HTML markup as is, simplify writing the test and more
    readable.

--- asciidoctor_test.go
+++ asciidoctor_test.go
@@ -4,10 +4,81 @@
 package asciidoctor
 
 import (
-	"os"
+	"bytes"
 	"testing"
+
+	"github.com/shuLhan/share/lib/test"
 )
 
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
+const (
+	outputCallHtmlWriteHeader = "htmlWriteHeader"
+	outputCallToHTML          = "ToHTML"
+	outputCallToHTMLBody      = "ToHTMLBody"
+)
+
+func TestData(t *testing.T) {
+	var (
+		listTData    []*test.Data
+		tdata        *test.Data
+		inputCall    string
+		outputCall   string
+		inputName    string
+		subtestName  string
+		inputContent []byte
+		err          error
+	)
+
+	listTData, err = test.LoadDataDir("testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tdata = range listTData {
+		inputCall = tdata.Flag["input_call"]
+		outputCall = tdata.Flag["output_call"]
+
+		for inputName, inputContent = range tdata.Input {
+			subtestName = tdata.Name + "/" + inputName
+
+			t.Run(subtestName, func(t *testing.T) {
+				var (
+					doc  *Document
+					bbuf bytes.Buffer
+					exp  []byte
+					got  []byte
+				)
+
+				exp = tdata.Output[inputName]
+
+				bbuf.Reset()
+
+				switch inputCall {
+				default:
+					doc = Parse(inputContent)
+				}
+
+				switch outputCall {
+				case outputCallHtmlWriteHeader:
+					htmlWriteHeader(doc, &bbuf)
+				case outputCallToHTML:
+					err = doc.ToHTML(&bbuf)
+				case outputCallToHTMLBody:
+					err = doc.ToHTMLBody(&bbuf)
+				default:
+					err = doc.ToHTMLEmbedded(&bbuf)
+				}
+				if err != nil {
+					got = []byte(err.Error())
+				} else {
+					// Since the data file is from file it
+					// always end with LF, we need to add
+					// new line to output.
+					bbuf.WriteByte('\n')
+					got = bbuf.Bytes()
+				}
+
+				test.Assert(t, subtestName, string(exp), string(got))
+			})
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ module git.sr.ht/~shulhan/asciidoctor-go
 
 go 1.18
 
-require github.com/shuLhan/share v0.39.0
+require github.com/shuLhan/share v0.39.1-0.20220723102219-eb932c941bd3
 
 //replace github.com/shuLhan/share => ../share
