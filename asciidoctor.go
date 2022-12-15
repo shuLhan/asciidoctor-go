60a55a32252e349f2bdfeace983c251ee5865fb6
Shulhan <ms@kilabit.info>
2022-12-16 00:59:35 +0700

    all: add package documentation and comment on Version

--- asciidoctor.go
+++ asciidoctor.go
@@ -1,12 +1,17 @@
 // SPDX-FileCopyrightText: 2020 M. Shulhan <ms@kilabit.info>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+// Package asciidoctor-go is the Go module to parse the [AsciiDoc markup].
+// Its currently support converting the asciidoc to HTML5.
+//
+// [AsciiDoc markup]: https://asciidoctor.org/docs/what-is-asciidoc
 package asciidoctor
 
 import "github.com/shuLhan/share/lib/math/big"
 
 const (
-	Version = `0.3.2`
+	// Version of this module.
+	Version = `0.3.3-dev`
 )
 
 func init() {
